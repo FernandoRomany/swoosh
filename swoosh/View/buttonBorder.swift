8681dbefa57b635af3190422858dc7a9ecfc02ee
Fernando Romany <fernandoromany@Fernandos-MacBook-Pro.local>
2018-03-11 03:02:30 +0200

    Create MVC folders and segue

--- swoosh/View/buttonBorder.swift
+++ swoosh/View/buttonBorder.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2018 Fernando Romany. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+@IBDesignable
+class buttonBorder : UIButton {
+    @IBInspectable var radious : CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = radious
+        }
+    }
+    @IBInspectable var border : CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = border
+        }
+    }
+    @IBInspectable var borderColor : UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
